754f1d9f9eb3154a5962866008908e0d7a90617e
EscapingKotaro <kokorinleonid83@gmail.com>
2025-09-26 06:50:59 +0300

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 class NavigationButton(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
