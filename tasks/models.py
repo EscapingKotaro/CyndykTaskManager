65f3713a2de7e752f70f4549debf2b338ca4a84a
EscapingKotaro <kokorinleonid83@gmail.com>
2025-09-27 19:44:52 +0300

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -29,7 +29,7 @@ class CustomUser(AbstractUser):
 class Task(models.Model):
     STATUS_CHOICES = [
         ('created', 'Создана'),
-        ('in_progress', 'В исполнении'),  # НОВЫЙ СТАТУС
+        ('in_progress', 'В исполнении'),
         ('submitted', 'Сдана на отчет'),
         ('completed', 'Выполнена'),
     ]
@@ -43,6 +43,7 @@ class Task(models.Model):
     payment_amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма к оплате', default=0)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='created', verbose_name='Статус')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата выдачи')
+    started_date = models.DateTimeField(null=True, blank=True, verbose_name='Дата начала работы')  # НОВОЕ ПОЛЕ
     submitted_date = models.DateTimeField(null=True, blank=True, verbose_name='Дата сдачи на отчет')
     created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, 
                                   related_name='created_tasks', verbose_name='Кем создана')
